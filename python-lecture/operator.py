f426ce63037de9881fb4de1e7a2d59cbbf34ab83
Chae Seong Lim <pan173434@gmail.com>
2024-06-16 18:25:39 +0900

    파이썬 기초 문법 연산자 이해

---
+++ python-lecture/operator.py
@@ -0,0 +1,45 @@
+# 산술 연산자
+num1 = 20
+num2 = 30
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 // num2)
+
+# 문자열 덧셈
+print('a' + 'b')
+
+# 비교 연산자
+print(10 == 200)
+print(10 > 200)
+print(10 >= 200)
+print(10 < 200)
+print(10 <= 200)
+print(10 != 200)
+
+# 문자열 비교
+print('c++' == 'python')
+print('c++' != 'python')
+
+# 아스키 코드 값
+print('c' > 'p')
+print('c' < 'p')
+
+# 논리 연산자 not, or ,and 연산자 있다
+print(True)
+print(not True)
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+bool1 = True
+bool2 = False
+print(bool1 and bool2)
+
+print((3 > 1) and (3 > 10))
+print((3 > 1) or (3 >= 10))
+print((3 == 1) or (3 >= 10))
+
