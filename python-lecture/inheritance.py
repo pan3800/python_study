7f52143c9e77462cd89680be7c266c874e03def0
Chae Seong Lim <pan173434@gmail.com>
2024-06-22 08:01:59 +0900

    파이썬 다중 상속 사용방법

--- python-lecture/inheritance.py
+++ python-lecture/inheritance.py
@@ -10,3 +10,31 @@ class ChildCls(ParantCls):
 
 chilcls = ChildCls()
 chilcls.printMethod()
+
+# 다중 상속
+class ParentCls1:
+    def __init__(self):
+        pass
+    def clsMethod1(self):
+        print('clsMethod')
+
+class ParentCls2:
+    def __init__(self):
+        pass
+    def clsMethod2(self):
+        print('clsMethod2')
+
+class ParentCls3:
+    def __init__(self):
+        pass
+    def clsMethod3(self):
+        print('clsMethod3')
+
+class ChildCls(ParentCls1, ParentCls2, ParentCls3):
+    def __init__(self):
+        pass
+
+chilcls = ChildCls()
+chilcls.clsMethod1()
+chilcls.clsMethod2()
+chilcls.clsMethod3()
