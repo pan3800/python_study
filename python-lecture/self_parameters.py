92e9652e88d1b8efee8e0f52278a4b49840d66e2
Chae Seong Lim <pan173434@gmail.com>
2024-06-21 15:26:03 +0900

    setDoubleMajor 배열 사용하기

--- python-lecture/self_parameters.py
+++ python-lecture/self_parameters.py
@@ -1,19 +1,29 @@
 class Student:
+
+    doubleMajor = []
+
     def __init__(self, name, age, gender, major):
                     self.name = name
                     self.age = age
                     self.gender = gender
                     self.major = major
+                    #self.doubleMajor = []
 
     def printStudentInfo(self):
         print('name : {0}'.format(self.name))
         print('age : {0}'.format(self.age))
         print('gender : {0}'.format(self.gender))
         print('major : {0}'.format(self.major))
+        print('school : {0}'.format(self.doubleMajor))
+
+    def setDoubleMajor(self, doubleMajor):
+        self.doubleMajor.append(doubleMajor)
 
 
 gildong = Student('Hong Gildong',20, 'M', '컴퓨터 공학')
-suji = Student('Lee Suji', 20, 'W', '연극 영화')
-
+gildong.setDoubleMajor('국문학')
 gildong.printStudentInfo()
+
+suji = Student('Lee Suji', 20, 'W', '연극 영화')
+suji.setDoubleMajor('영문학')
 suji.printStudentInfo()
