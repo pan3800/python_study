74d5cea27bb57f38025404a4ec845c546180aeb4
Chae Seong Lim <pan173434@gmail.com>
2024-06-23 15:25:37 +0900

    ex_13_1.py -> module import 사용방법 예시

--- python-lecture/module/ex_13_1.py
+++ python-lecture/module/ex_13_1.py
@@ -1,8 +1,22 @@
 # module
 # 소스를 부품화해서 필요할 때 import 한 후 사용한다.
-import cooking
+# import cooking
 
-cooking.makeJjajang()
-cooking.makeJjamppong()
-cooking.makePasta()
-cooking.makePizza()
+from cooking import makeJjajang
+from cooking import makeJjamppong
+from cooking import makePasta
+from cooking import makePizza
+
+from cooking import *
+
+makeJjajang()
+makeJjamppong()
+makePasta()
+makePizza()
+
+
+# cooking.makeJjajang()
+# cooking.makeJjamppong()
+# cooking.makePasta()
+# cooking.makePizza()
+
