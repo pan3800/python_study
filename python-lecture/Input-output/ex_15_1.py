e4f997c4719fdbd25666a4faf8e87a048dd2638c
Chae Seong Lim <pan173434@gmail.com>
2024-06-25 12:03:59 +0900

    ex_15_1.py 파일 수정

--- python-lecture/Input-output/ex_15_1.py
+++ python-lecture/Input-output/ex_15_1.py
@@ -1,5 +1,5 @@
 # 파일 생성 방법
-
+# 파이썬은 파일을 읽고 쓰기 위한 함수로 open(), write(), read(), close() 함수를 제공한다.
 # f = open('/Users/imchaeseong/Documents/python/python_study/python-lecture/Input-output/textFile.txt', 'w')
 # f.write('hello python')
 # f.close()
