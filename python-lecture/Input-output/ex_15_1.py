a6ba65d930db890de000ef1addc8cb4ea9507371
Chae Seong Lim <pan173434@gmail.com>
2024-06-25 11:52:37 +0900

    데이터 입출력 -> 파일 읽기 방법

--- python-lecture/Input-output/ex_15_1.py
+++ python-lecture/Input-output/ex_15_1.py
@@ -1,3 +1,12 @@
-f = open('/Users/imchaeseong/Documents/python/python_study/python-lecture/Input-output/textFile.txt', 'w')
-f.write('hello python')
+# 파일 생성 방법
+
+# f = open('/Users/imchaeseong/Documents/python/python_study/python-lecture/Input-output/textFile.txt', 'w')
+# f.write('hello python')
+# f.close()
+
+# 파일 읽기 방법
+f = open('/Users/imchaeseong/Documents/python/python_study/python-lecture/Input-output/textFile.txt', 'r')
+print('f.read() : {0}'.format(f.read()))
 f.close()
+
+
